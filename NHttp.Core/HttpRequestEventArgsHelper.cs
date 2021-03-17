ff08120c78f79fe1325226e77331b402d832b05b
Xiaoyu <xiaoyu.vax@qq.com>
2021-03-17 11:19:44 +0800

    Comments

--- NHttp.Core/HttpRequestEventArgsHelper.cs
+++ NHttp.Core/HttpRequestEventArgsHelper.cs
@@ -6,14 +6,19 @@ namespace NHttp
     {
 
         /// <summary>
-        /// 读取参数
+        /// Shortcut for extracting Http Params
         /// </summary>
         /// <param name="e"></param>
         /// <param name="str"></param>
         /// <returns></returns>
         public static string GetParam(this HttpRequestEventArgs e, string str) => e.Request.GetParam(str);
 
-
+        /// <summary>
+        /// Shortcut for extracting Http Params
+        /// </summary>
+        /// <param name="req"></param>
+        /// <param name="str"></param>
+        /// <returns></returns>
         public static string GetParam(this HttpRequest req, string str) => req != null && req.Params.AllKeys.Contains(str) ? req.Params[str]?.ToString().Trim() : null;
     }
 }
